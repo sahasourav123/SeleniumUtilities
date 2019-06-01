ef78955b15405e3b55772c736d4a4b2f315dea93
Dell\SouravS <sahasourav123@gmail.com>
2019-06-01 12:36:04 +0530

    Create ThreadData Object while creating driver.

---
+++ src/main/java/pojo/Keywords.java
@@ -0,0 +1,19 @@
+package pojo;
+
+public enum Keywords {
+	Name,
+	Id,
+	Xpath,
+	CSSSelector,
+	Class,
+	ClassName,
+	IdContains,
+	NameContains,
+	IdStartWith,
+	IdEndWith,
+	LinkText,
+	PartialLinkText,
+	TagName,
+	NameStartWith,
+	NameEndWith;
+}
